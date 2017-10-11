9c6a68c862ca402127876a3d8c7e2fdd75e66cec
Jesse_thinkpad <331072550@qq.com>
2017-10-11 11:09:09 +0800

    按小时产生出数

--- wg-generator/src/main/java/org/iconsider/generator/GeneratorByTime.java
+++ wg-generator/src/main/java/org/iconsider/generator/GeneratorByTime.java
@@ -54,7 +54,6 @@ public class GeneratorByTime {
             }
             generator.generate(sdf.format(new Date(beginTimestamp)));
             beginTimestamp = beginTimestamp + 20*60*1000L;
-
         }
 
         System.exit(0);
@@ -73,14 +72,14 @@ public class GeneratorByTime {
             r0.setSectionName(report.getSectionName());
             r0.setDirection(report.getPositiveDirection());
             r0.setGuest(generateGuest(dateTime));
-            r0.setSpeed(new Random().nextInt(50) + 60 + (new Random().nextDouble()));
+            r0.setSpeed(generateSpeed(dateTime));
             Report r1 = new Report();
             r1.setTime(dateTime);
             r1.setHighwayName(report.getHighwayName());
             r1.setSectionName(report.getSectionName());
             r1.setDirection(report.getNegative_direction());
             r1.setGuest(generateGuest(dateTime));
-            r1.setSpeed(new Random().nextInt(50) + 60 + (new Random().nextDouble()));
+            r1.setSpeed(generateSpeed(dateTime));
 
             resultList.add(r0);
             resultList.add(r1);
@@ -101,49 +100,79 @@ public class GeneratorByTime {
 
             switch (hour) {
                 case 0:
-                    guest = new Random().nextInt(150) + 150;
+                    guest = new Random().nextInt(150) + 300;
                     break;
                 case 1:
-                    guest = new Random().nextInt(100) + 100;
+                    guest = new Random().nextInt(150) + 250;
                     break;
                 case 2:
-                    guest = new Random().nextInt(100) + 100;
+                    guest = new Random().nextInt(100) + 250;
                     break;
                 case 3:
-                    guest = new Random().nextInt(100) + 80;
+                    guest = new Random().nextInt(100) + 200;
                     break;
                 case 4:
-                    guest = new Random().nextInt(100) + 50;
+                    guest = new Random().nextInt(100) + 100;
                     break;
                 case 5:
-                    guest = new Random().nextInt(100) + 80;
+                    guest = new Random().nextInt(100) + 50;
                     break;
                 case 6:
                     guest = new Random().nextInt(100) + 100;
                     break;
                 case 7:
-                    guest = new Random().nextInt(150) + 150;
+                    guest = new Random().nextInt(100) + 100;
                     break;
                 case 8:
                     guest = new Random().nextInt(150) + 150;
                     break;
+                case 9:
+                    guest = new Random().nextInt(150) + 200;
+                    break;
+                case 10:
+                    guest = new Random().nextInt(150) + 300;
+                    break;
+                case 11:
+                    guest = new Random().nextInt(150) + 400;
+                    break;
                 case 12:
-                    guest = new Random().nextInt(400) + 190;
+                    guest = new Random().nextInt(150) + 500;
                     break;
                 case 13:
-                    guest = new Random().nextInt(400) + 190;
+                    guest = new Random().nextInt(150) + 600;
                     break;
                 case 14:
-                    guest = new Random().nextInt(400) + 190;
+                    guest = new Random().nextInt(150) + 500;
                     break;
                 case 15:
-                    guest = new Random().nextInt(400) + 190;
+                    guest = new Random().nextInt(150) + 500;
                     break;
                 case 16:
-                    guest = new Random().nextInt(400) + 190;
+                    guest = new Random().nextInt(150) + 500;
+                    break;
+                case 17:
+                    guest = new Random().nextInt(150) + 500;
+                    break;
+                case 18:
+                    guest = new Random().nextInt(150) + 500;
+                    break;
+                case 19:
+                    guest = new Random().nextInt(150) + 500;
+                    break;
+                case 20:
+                    guest = new Random().nextInt(150) + 500;
+                    break;
+                case 21:
+                    guest = new Random().nextInt(150) + 400;
+                    break;
+                case 22:
+                    guest = new Random().nextInt(150) + 350;
+                    break;
+                case 23:
+                    guest = new Random().nextInt(150) + 300;
                     break;
                 default:
-                    guest = new Random().nextInt(300) + 150;
+                    guest = new Random().nextInt(150) + 300;
                     break;
             }
         } catch (ParseException e) {
@@ -151,10 +180,102 @@ public class GeneratorByTime {
         }
         return guest;
     }
+    public double generateSpeed(String dateTime) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        double speed = 100;
+        try {
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(sdf.parse(dateTime));
+            int hour = calendar.get(Calendar.HOUR_OF_DAY);
+
+            switch (hour) {
+                case 0:
+                    speed = new Random().nextInt(30) + 30 + (new Random().nextDouble());
+                    break;
+                case 1:
+                    speed = new Random().nextInt(20) + 30 + (new Random().nextDouble());
+                    break;
+                case 2:
+                    speed = new Random().nextInt(20) + 30 + (new Random().nextDouble());
+                    break;
+                case 3:
+                    speed = new Random().nextInt(30) + 30 + (new Random().nextDouble());
+                    break;
+                case 4:
+                    speed = new Random().nextInt(30) + 40 + (new Random().nextDouble());
+                    break;
+                case 5:
+                    speed = new Random().nextInt(40) + 50 + (new Random().nextDouble());
+                    break;
+                case 6:
+                    speed = new Random().nextInt(40) + 50 + (new Random().nextDouble());
+                    break;
+                case 7:
+                    speed = new Random().nextInt(30) + 50 + (new Random().nextDouble());
+                    break;
+                case 8:
+                    speed = new Random().nextInt(20) + 45 + (new Random().nextDouble());
+                    break;
+                case 9:
+                    speed = new Random().nextInt(20) + 40 + (new Random().nextDouble());
+                    break;
+                case 10:
+                    speed = new Random().nextInt(20) + 30 + (new Random().nextDouble());
+                    break;
+                case 11:
+                    speed = new Random().nextInt(20) + 30 + (new Random().nextDouble());
+                    break;
+                case 12:
+                    speed = new Random().nextInt(20) + 20 + (new Random().nextDouble());
+                    break;
+                case 13:
+                    speed = new Random().nextInt(20) + 20 + (new Random().nextDouble());
+                    break;
+                case 14:
+                    speed = new Random().nextInt(10) + 20 + (new Random().nextDouble());
+                    break;
+                case 15:
+                    speed = new Random().nextInt(10) + 20 + (new Random().nextDouble());
+                    break;
+                case 16:
+                    speed = new Random().nextInt(10) + 20 + (new Random().nextDouble());
+                    break;
+                case 17:
+                    speed = new Random().nextInt(10) + 20 + (new Random().nextDouble());
+                    break;
+                case 18:
+                    speed = new Random().nextInt(10) + 30 + (new Random().nextDouble());
+                    break;
+                case 19:
+                    speed = new Random().nextInt(10) + 30 + (new Random().nextDouble());
+                    break;
+                case 20:
+                    speed = new Random().nextInt(10) + 30 + (new Random().nextDouble());
+                    break;
+                case 21:
+                    speed = new Random().nextInt(10) + 30 + (new Random().nextDouble());
+                    break;
+                case 22:
+                    speed = new Random().nextInt(10) + 30 + (new Random().nextDouble());
+                    break;
+                case 23:
+                    speed = new Random().nextInt(20) + 30 + (new Random().nextDouble());
+                    break;
+                default:
+                    speed = new Random().nextInt(20) + 30 + (new Random().nextDouble());
+                    break;
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return speed;
+    }
 
     public List<Report> queryForSection() {
         String sql = "SELECT highway_name,section_name,positive_direction,negative_direction FROM d_hx_highway_cell GROUP BY section_name,highway_name,positive_direction,negative_direction";
-        JdbcTemplate template = SpringContextInstance.getBean("oracleJdbcTemplate", JdbcTemplate.class);
+//        JdbcTemplate template = SpringContextInstance.getBean("oracleJdbcTemplate", JdbcTemplate.class);
+        JdbcTemplate template = SpringContextInstance.getBean("pgJdbcTemplate", JdbcTemplate.class);
         SqlRowSet sqlRowSet = template.queryForRowSet(sql);
 
         List<Report> list = new ArrayList<Report>();
@@ -178,19 +299,18 @@ public class GeneratorByTime {
         }
 
         if("test".equals(GeneratorEvery20Mins.env)) {
-            //测试环境
-//          sql = "INSERT INTO f_hx_highway_statistic_20m (start_time, highway_name, section_name, direction, guest_count, speed) VALUES (to_timestamp(?,'YYYY-MM-DD HH24:MI:SS'),?,?,?,?,?)";
-            sql = "INSERT INTO temp_hx_highway_result2 (start_time, highway_name, section_name, direction, guest_count, speed) VALUES (?,?,?,?,?,?)";
+          //本地pg数据库
+          sql = "INSERT INTO f_hx_highway_statistic_20m (start_time, highway_name, section_name, direction, guest_count, speed) VALUES (to_timestamp(?,'YYYY-MM-DD HH24:MI:SS'),?,?,?,?,?)";
+          //测试表
+//            sql = "INSERT INTO temp_hx_highway_result2 (start_time, highway_name, section_name, direction, guest_count, speed) VALUES (?,?,?,?,?,?)";
         }
         if("product".equals(GeneratorEvery20Mins.env)) {
             //生产环境
             sql = "INSERT INTO f_hx_highway_statistic_20m (start_time, highway_name, section_name, direction, guest_count, speed) VALUES (?,?,?,?,?,?)";
         }
 
-        JdbcTemplate template = SpringContextInstance.getBean("oracleJdbcTemplate", JdbcTemplate.class);
-//        for (Report r : list) {
-//            template.update(sql, r.getTime(), r.getHighwayName(), r.getSectionName(), r.getDirection(), r.getGuest(), r.getSpeed());
-//        }
+//        JdbcTemplate template = SpringContextInstance.getBean("oracleJdbcTemplate", JdbcTemplate.class);
+        JdbcTemplate template = SpringContextInstance.getBean("pgJdbcTemplate", JdbcTemplate.class);
 
         template.batchUpdate(sql, new BatchPreparedStatementSetter() {
             public void setValues(PreparedStatement ps, int i) throws SQLException {
