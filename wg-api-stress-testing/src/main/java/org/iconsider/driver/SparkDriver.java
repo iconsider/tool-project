6d4266cd3effda9b7f114507b919563d13b4072b
Jesse_thinkpad <331072550@qq.com>
2017-11-02 18:57:40 +0800

    添加kerberos认证

--- wg-api-stress-testing/src/main/java/org/iconsider/driver/SparkDriver.java
+++ wg-api-stress-testing/src/main/java/org/iconsider/driver/SparkDriver.java
@@ -1,10 +1,40 @@
 package org.iconsider.driver;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.spark.SparkConf;
+import org.iconsider.job.SparkJob;
+
+import java.io.IOException;
+
 /**
  * Created by liuzhenxing on 2017-10-24.
  */
 public class SparkDriver {
     public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("plz input args");
+            return;
+        }
+        System.out.println(String.format("my args: %s", args[0]));
+        SparkConf conf = new SparkConf();
+        SparkDriver.spark_kerberos();
+        SparkJob job = new SparkJob();
+        job.setPath(args[0]);
+        job.execute(conf);
+    }
 
+    //kerberos验证
+    public static void spark_kerberos() {
+        try {
+            Configuration conf = new Configuration();
+            System.setProperty("java.security.krb5.conf", "/etc/krb5.conf");
+            UserGroupInformation.setConfiguration(conf);
+            UserGroupInformation.loginUserFromKeytab("hanxinnil", "/home/hanxinnil/hanxinnil.keytab");
+            System.out.println("kerberos in highway spark, 认证成功(seccess)");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("kerberos in highway spark, 认证失败(fail)");
+        }
     }
 }
--- wg-api-stress-testing/src/main/java/org/iconsider/job/SparkJob.java
+++ wg-api-stress-testing/src/main/java/org/iconsider/job/SparkJob.java
@@ -1,7 +1,36 @@
 package org.iconsider.job;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+
 /**
  * Created by liuzhenxing on 2017-10-24.
  */
-public class SparkJob {
+public class SparkJob implements Serializable {
+    private static final long serialVersionUID = 5199871376840325353L;
+    private static final Logger LOG = LoggerFactory.getLogger(SparkJob.class);
+    private String path = "";
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void execute(SparkConf sparkConf) {
+        JavaSparkContext javaSparkContext = new JavaSparkContext(sparkConf);
+        JavaRDD<String> fiveMinRecordRDD = javaSparkContext.textFile(path);
+        fiveMinRecordRDD.foreach(new VoidFunction<String>() {
+            @Override
+            public void call(String s) throws Exception {
+                System.out.println(String.format("my lines: %s", s));
+                LOG.info("log lines: {}", s);
+            }
+        });
+        System.out.println(String.format("spark end"));
+    }
 }
